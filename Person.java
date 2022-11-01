cc7c7ead231e8cf20f0c45794a2c056ea8ea20ff
Jeppe <jetho22@student.sdu.dk>
2022-11-01 09:20:18 +0100

    test

--- Person.java
+++ Person.java
@@ -45,6 +45,7 @@ public class Person {
     }
 
     // Main method for testing purposes
+    // test
     public static void main(String[] args) {
         Person npc1 = new Person("Hans", "Hi, my name is Hans, 68 years old and I'm retired");
         //npc1.setEngaged();
